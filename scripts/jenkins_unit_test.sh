90aca316cd3b80acef876adfb60737cc5a7b31e0
Andy Liu <andyliuliming@outlook.com>
2018-11-23 10:46:57 +0800

    add linter check for the source code.

--- scripts/jenkins_unit_test.sh
+++ scripts/jenkins_unit_test.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-set -e
+set -euo pipefail
 project_name=$(echo $GIT_URL | awk -F/ '{print $NF}' | sed 's/.git//g')
 mkdir -p $GOPATH/src/github.com/Microsoft
 mkdir -p $GOPATH/src/github.com/Microsoft/${project_name}
 cp -r $WORKSPACE/* $GOPATH/src/github.com/Microsoft/${project_name}/
+exit_code=0
+
 pushd $GOPATH/src/github.com/Microsoft/${project_name}
     rm -rf ./src
+
+    ./scripts/validate_go.sh || exit_code=1
     go get -u golang.org/x/lint/golint
     go get -u github.com/onsi/ginkgo/ginkgo
     go get -u github.com/onsi/gomega/...
     export PATH="$PATH:$GOPATH/bin"
-    ginkgo -race -r
+    ginkgo -race -r || exit_code=1
+    exit $exit_code
 popd
